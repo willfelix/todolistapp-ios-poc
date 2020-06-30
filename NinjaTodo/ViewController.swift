765f84a48c30223b0828789e65385811b4e487ce
Will Felix <willfelix93@gmail.com>
2020-06-29 21:21:42 -0300

    Initial Commit

---
+++ NinjaTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NinjaTodo
+//
+//  Created by Will Felix on 29/06/20.
+//  Copyright © 2020 Will Felix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
